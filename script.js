712b79b96bc0b512bd08040d13348b4231ac219a
melloyasminn <melloyasminm@gmail.com>
2023-12-06 10:18:00 -0300

    ;

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function add() {
   let input_text = document.createTextNode(input_value);
 
   li.appendChild(input_text);
-  document.querySelector("ol").appendChild(li);
+  document.querySelector("ul").appendChild(li);
   document.form_main.task.value = "";
   //aqui cria um botão de fechar pra cada elemento colocado na lista
   createCloseButton(li);
@@ -17,7 +17,7 @@ function createCloseButton(li) {
   // criei um span que esconde o elemento
   let span = document.createElement("SPAN");
   // aqui é o "x" que aparece em todos os elementos da lista, e que faz o elemento ser escondido ao clicar
-let txt = document.createTextNode(	"\u2A02");
+  let txt = document.createTextNode("\u2A02");
 
   span.className = "close";
   span.appendChild(txt);
@@ -26,9 +26,9 @@ let txt = document.createTextNode(	"\u2A02");
   span.onclick = () => (span.parentElement.style.display = "none");
 }
 
-
-document.querySelectorAll("li").forEach(createCloseButton);
+document.querySelectorAll('li').forEach(createCloseButton);
 //quando clica no elemento da lista, faz com que ele apareça riscado como tarefa feita0
-document.querySelector("ul").addEventListener("click", (e) => {
-  if (e.target.tagName === "li") e.target.classList.toggle("checked");
+document.querySelector('ul').addEventListener('click', (e) => {
+  if (e.target.tagName === 'LI')
+    e.target.classList.toggle('checked');
 });
